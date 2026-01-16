0471c4ab3336a63b3d8facb6b93e3af25e69625c
princeku1456 <princeiitk9708@gmail.com>
2026-01-16 17:04:43 +0530

    fixed

--- auth.js
+++ auth.js
@@ -22,6 +22,7 @@ let isPracticeMode = false; // Isse global banayein hain taaki practice.js mein 
 const quizDataCache = {};
 let userHistory = [];
 let confidenceChartInstance = null; //
+let globalConfidenceChartInstance = null; //
 let dashboardDataLoaded = false;
 let globalStatsCache = {};
 let leaderboardCache = {};
