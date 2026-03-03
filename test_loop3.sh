2a2da91bb415154eb43f9a57ed41ff3c7a2fad66
Random <63698575+princeku1456@users.noreply.github.com>
2026-03-03 10:50:56 +0530

    Delete test_loop3.sh

--- test_loop3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-total=0
-runs=5
-for i in $(seq 1 $runs); do
-  output=$(node bench3.js | grep "Render time for chapters:")
-  val=$(echo $output | grep -o "[0-9.]\+" | head -1)
-  echo "Run $i: $val ms"
-done
